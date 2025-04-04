4c1ceec0fdeddf3ac7e52c5244f9440e4b77a870
seriouslyvictor <schiavovictor@outlook.com>
2025-04-03 23:09:30 -0300

    versão inicial do primeiro módulo do curso

---
+++ variaveis.py
@@ -0,0 +1,43 @@
+print("Hello World")
+
+#printamos nossa primeira string, cadeia de caracteres!
+#strings são facilmente reconhecidas pelas aspas envolvendo o texto/número.
+
+print("Hello World\nHello World\nHello World" )
+# Aqui temos um caracter de escape, o \n signifca quebra de linha.
+
+print("Tibúrcio" + " é um belo nome!")
+# Essa ação é conhecida como concatenação de strings, a "soma" de palavras
+
+# !!!! ATENÇÃO, espaços em programação, especialmente em python, NÃO pode ser desconsiderados, espaços indicam identação e são usados no lugar das {} em outras linguagens. !!!!
+
+# Como receber informação do usuário? R: função input()
+input("Qual seu VULGO?")
+
+# Mas para onde vai a informação que o usuário digitou? R: perdido forever, a não ser que....
+# VARIABLES!!!
+
+# Variáveis são "caixas" que armazenam dados para serem manipulados ou resgatados posteriormente.
+# Variáveis em Python não são "tipadas", isso é, não precisamos definir o tipo de informação que será inserido na "caixa"
+nome = input("Qual seu nome, amigo?")
+print(nome)
+
+nome = "LINELSON"
+print(nome)
+
+# Vibecheck:
+# Troque os conteúdos dessas duas variáveis:
+# Você só pode criar mais UMA variável para realizar essa operação.
+# O conteúdo das variáveis não pode ser perdido
+
+copo1 = "Água"
+copo2 = "Vinho"
+
+
+# Regras para nomes de variáveis
+# Ideia geral = Nomes de variáveis significativos, que facilitem a leitura do código.
+# Não é possível usar espaços - caso queira separar as palavras, use o snake_case
+# Não é possível iniciar variáveis com NÚMEROS, mas podemos usar números no final.
+# Não é possível usar nomes reservados para variáveis ex: print, input
+
+# Kahoot de vibe check
