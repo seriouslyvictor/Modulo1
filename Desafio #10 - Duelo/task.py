6fbf0a6247a9da63d8521ef7e097e618c5a00ead
seriouslyvictor <schiavovictor@outlook.com>
2025-05-09 22:53:43 -0300

    Adicionado Desafio #11

--- Desafio #10 - Duelo/task.py
+++ Desafio #10 - Duelo/task.py
@@ -6,19 +6,20 @@
 
 # TODO: 4 - Defina a função verificar_resposta
 # Essa função deve retornar True se o palpite estiver certo, False caso contrário
+# Alerta, essa função exige uma lógica relativamente complicada, considere que a resposta do usuário pode ser A ou B
 
 # TODO: 5 - Imprima o logo do jogo
 
 # TODO: 6 - Inicialize as variáveis: pontos = 0 e executando = True
 
-# TODO: 7 - Sorteie um termo inicial para termo_b usando random.choice(data)
+# TODO: 7 - Sorteie os termos iniciais
 
 # TODO: 8 - Inicie o loop principal do jogo: while executando
 
     # TODO: 9 - Garanta que termo_a e termo_b não sejam iguais (se forem, sorteie outro termo_b)
 
-    # TODO: 10 - Exiba os dois termos formatados usando format_data()
-    # Exemplo: Termo A: vs Termo B
+    # TODO: 10 - Exiba os dois termos formatados
+    # Exemplo: Termo A: nome e descrição vs Termo B: nome e descrição
 
     # TODO: 11 - Solicite um palpite do usuário ("a" ou "b")
     # Dica: use um while para garantir que o input seja válido
