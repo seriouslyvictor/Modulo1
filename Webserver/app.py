4a49736b71783024960cae9f98a038ad2b402b03
seriouslyvictor <schiavovictor@outlook.com>
2025-05-30 20:34:11 -0300

    adicionado templates jinja

--- Webserver/app.py
+++ Webserver/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import json, os
 app = Flask(__name__)
 
+users =[{"perfil": "Thea", "seguidores": 50, "seguindo": 250, "publicações": 38}, {"perfil": "Theo", "seguidores": 150, "seguindo": 5, "publicações": 50}]
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/users')
+def display_users(profiles=None):
+    return render_template("users.html", profiles=users)
+
+@app.route("/users/<username>")
+def display_user(username):
+    user = None
+    for item in users:
+        if item["perfil"].lower() == username.lower():
+            user = item
+            break
+    if user:
+        return render_template("single.html", user=user)
+    else:
+        return render_template("index.html")
 
 @app.route("/submit", methods=["POST", "GET"])
 def receber_dados():
